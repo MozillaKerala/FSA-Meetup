a6c0d6d520bdba6ed42b4ab8388013cea52b0987
shinenelson <radioactive.life@gmail.com>
2015-02-01 13:09:26 +0530

    Smooth scroll effect for Register button

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
 	app.el['back-to-top']				= $('.back-to-top');
 	app.el['html-body']					= $('html,body');
 	app.el['accordion']					= $('#accordion');
+	app.el['reg-btn']					= $('.reg-btn');
+	app.el['register']					= $('#register');
+    
+	//Scroll cover Register button to Registration section
+		var pos = app.el['register'].position();
+		app.el['reg-btn'].click(function () {
+      app.el['html-body'].animate({
+        scrollTop: pos.top
+      }, 1500);
+      return false;
+    });
 
     //Change glyphicon on accordion on toggle
     app.el['accordion'].on('hide.bs.collapse show.bs.collapse', function (n) {
