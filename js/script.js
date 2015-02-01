919773c0b388814eb95f1f6dff67619c3124917d
shinenelson <radioactive.life@gmail.com>
2015-02-01 13:03:05 +0530

    Back to Top and script adaptions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){    
-    
-    $('#accordion').on('hide.bs.collapse show.bs.collapse', function (n) {
+$(document).ready(function(){
+
+	// Init global DOM elements, functions and arrays
+	window.app							= {el : {}, fn : {}};
+	app.el['window']					= $(window);
+	app.el['document']					= $(document);
+	app.el['back-to-top']				= $('.back-to-top');
+	app.el['html-body']					= $('html,body');
+	app.el['accordion']					= $('#accordion');
+
+    //Change glyphicon on accordion on toggle
+    app.el['accordion'].on('hide.bs.collapse show.bs.collapse', function (n) {
     	$(n.target).siblings('.panel-heading').find('span.glyphicon')
     	.toggleClass('glyphicon-plus glyphicon-minus');
     });
+
+	//Fade in .back-to-top
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 640) {
+			app.el['back-to-top'].fadeIn();
+		} else {
+			app.el['back-to-top'].fadeOut();
+		}
+	});
+
+	//Scroll to Top
+	app.el['back-to-top'].click(function () {
+		app.el['html-body'].animate({
+			scrollTop: 0
+		}, 1500);
+		return false;
+	});
 
 });
